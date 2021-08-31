a9795fd743f2a1cb59f7b2b4e6830eecf4f00550
JoaoAJMatos <joaoafonsoj2004@gmail.com>
2021-08-31 19:26:12 +0100

    Actual Initial Commit
    Implemented the main logic of the connection between sockets. The server is now able to listen for incomming connections. The client can connect to the server.

--- server.py
+++ server.py
@@ -1,4 +1,15 @@
 import socket
+import termcolor
+
+HOST = '127.0.0.1'
+PORT = 5555
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(('127.0.0.1'))
+sock.bind((HOST, PORT))
+
+print(termcolor.colored(f'[+] Server listening on port [{PORT}] for incoming connections', 'green'))
+
+sock.listen(5)
+
+target, ip = sock.accept()
+print(termcolor.colored(f'[+] Connection astablished with [{str(ip[0])}] port [{str(ip[1])}]!', 'green'))
